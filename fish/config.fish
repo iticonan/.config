72a04e84efa91ebc36abbe2b2945af2b7e2f8a74
Ivan Ticona <iticonan@gmail.com>
2024-02-19 12:32:03 -0500

    sway+waybar+sworkstyle+keyd setup

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+set -U fish_user_abbreviations $fish_user_abbreviations 'ls=ls -sh' 
+set fish_prompt_pwd_dir_length 0
+bind \cd 'exit'bind \cd 'exit'
+export PATH="$HOME/bin/symlinks:$HOME/.symfony5/bin:$PATH"
+fish_vi_key_bindings
