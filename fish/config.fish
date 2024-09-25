fbd8cf18963547b89d87883a530c4f63c3846cee
Ivan Ticona <iticonan@gmail.com>
2024-09-25 18:29:38 -0500

    changes to reflex kanata endgame (tbd?) layout

--- fish/config.fish
+++ fish/config.fish
@@ -2,12 +2,16 @@ abbr ltp "tmuxp load ."
 abbr ls "ls -sh"
 set -g fish_greeting
 set fish_prompt_pwd_dir_length 0
-bind \cd 'exit'bind \cd 'exit'
+bind \cd 'exit'
 export PATH="$HOME/bin/symlinks:$HOME/.symfony5/bin:$PATH"
-export LD_LIBRARY_PATH="/usr/local/KDAB/KDSOAP-2.2.95/lib:$LD_LIBRARY_PATH"
 fish_vi_key_bindings
 bind --mode insert \e\[3\;5~ 'kill-word'
 bind --mode insert \cW 'backward-kill-word'
+bind --erase --preset -k sright
+bind --erase --preset -k sleft
+bind --erase --preset -k sr
+bind --erase --preset -k sf
+
 
 zoxide init --cmd cd fish | source
 
